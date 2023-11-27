fdbb3d80c6935791042c125def45a8818cc759c2
AngieJoya99 <angie.joya@correounivalle.edu.co>
2023-11-27 14:09:45 -0500

    Avances GUI servidor

--- Servidor/src/Vista/GUIServidor.java
+++ Servidor/src/Vista/GUIServidor.java
@@ -6,7 +6,7 @@ public class GUIServidor extends JFrame
 {
     JTabbedPane pestanas;   
     JPanel pCrear, pVisualizar, pInformes, pHacer;
-    JScrollPane scVisualizar, scInforme;
+    JScrollPane spVisualizar, spInforme;
 
     JLabel lCrearNombre, lCrearArchivo, lCrearDuracion, 
         lIniciarCantidad, lIniciarTiempo, lIniciarTiempoRestante, 
@@ -34,9 +34,56 @@ public class GUIServidor extends JFrame
         setResizable(false);
     }
 
+    /**
+     * Crea e inicializa los elementos de la GUI
+     */
     public void crearGUI()
     {
         //Crear componentes
+        lCrearNombre = new JLabel("Nombre del examen");
+        lCrearArchivo= new JLabel("El archivo seleccionado fue: ");
+        lCrearDuracion = new JLabel("Duración del examen");
+
+        lIniciarCantidad = new JLabel("Cantidad de preguntas");
+        lIniciarCliente1 = new JLabel();
+        lIniciarCliente2 = new JLabel();
+        lIniciarCliente3 = new JLabel();
+        lIniciarPreguntas = new JLabel("Cantidad de preguntas");
+        lIniciarTiempo = new JLabel("Tiempo");
+        lIniciarTiempoRestante = new JLabel("Tiempo restante");
+        lIniciarRespondidas = new JLabel("Preguntas respondidas");
+
+        tfCrearNombre = new JTextField();
+        
+        bCrearCargar = new JButton("Cargar archivo");
+        bCrearCrear = new JButton("Crear");
+
+        bVerVer = new JButton("Ver");
+        bVerLimpiar = new JButton("Limpiar");
+
+        bInformeVer = new JButton("Ver");
+        bInformeLimpiar = new JButton("Limpiar");
+        bIniciarIniciar = new JButton("Iniciar");
+
+        taInforme = new JTextArea();
+        taVisualizar = new JTextArea();
+
+        cbInforme = new JComboBox<>();
+        cbIniciar = new JComboBox<>();
+        cbVisualizar = new JComboBox<>();
+
+        horas = new JSpinner();
+        minutos = new JSpinner();
+        segundos = new JSpinner();
+
+        spInforme = new JScrollPane();
+        spVisualizar= new JScrollPane();
+
+        pCrear = new JPanel();
+        pHacer = new JPanel();
+        pInformes = new JPanel();
+        pVisualizar = new JPanel();
+
         pestanas = new JTabbedPane();
 
         pestanas.addTab("Crear examen",pCrear);
